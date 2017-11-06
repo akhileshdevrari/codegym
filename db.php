e14d20fca094d2795b2a55dc8acce9bd6aaf137c
Anant Mishra <b16011@students.iitmandi.ac.in>
2017-11-07 00:22:22 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,6 @@
 		$username = "root";
 		$password = "mi";
 		$dbname = "codegym";
-		echo "e na ho pi";
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
 		if($conn->connect_error)
 		{
